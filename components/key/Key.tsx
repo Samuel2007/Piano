3130eb96e8374fbceb8ecd3990375d4aefacb39e
samuel <samuel.cwiek@wp.pl>
2024-10-18 17:12:24 +0200

    added black keys

--- components/key/Key.tsx
+++ components/key/Key.tsx
@@ -1,13 +1,37 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import React from "react";
 
 type KeyProps = {
   note: string;
+  index: number;
 };
 
-const Key = ({ note }: KeyProps) => {
+const Key = ({ note, index }: KeyProps) => {
+  const isBlackKey = note.includes("#");
+
+  function getTopOffeset(note: string) {
+    if (isBlackKey) {
+      switch (index) {
+        case 1:
+          return 75;
+        case 3:
+          return 175;
+        case 6:
+          return 375;
+        case 8:
+          return 475;
+        case 10:
+          return 575;
+      }
+    }
+
+    return 1000;
+  }
+
+  const styles = getStyles(getTopOffeset(note)); // 70 -> topOffset
+
   return (
-    <View style={styles.container}>
+    <View style={isBlackKey ? styles.blackKey : styles.whiteKey}>
       <Text>{note}</Text>
     </View>
   );
@@ -15,12 +39,24 @@ const Key = ({ note }: KeyProps) => {
 
 export default Key;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const getStyles = (topOffset: number) =>
+  StyleSheet.create({
+    whiteKey: {
+      height: 100,
+      borderWidth: 1,
+      borderColor: "black",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    blackKey: {
+      position: "absolute",
+      top: topOffset,
+      height: 50,
+      left: 100,
+      width: 500,
+      borderWidth: 1,
+      backgroundColor: "black",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
--- components/keyboard/Keyboard.tsx
+++ components/keyboard/Keyboard.tsx
@@ -2,12 +2,26 @@ import { View, Text } from "react-native";
 import React from "react";
 import Key from "../key/Key";
 
-const notes = ["C", "D", "E", "F", "G", "A", "B", "C"];
+const notes = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+  "C",
+];
 export default function Keyboard() {
   return (
     <View style={{ flex: 1, width: "100%" }}>
-      {notes.map((key) => {
-        return <Key note={key} />;
+      {notes.map((key, index) => {
+        return <Key note={key} index={index} />;
       })}
     </View>
   );
